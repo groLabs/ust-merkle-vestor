23d782f7f5576bbbd1e735ca444b2b39874f19f7
farhaan-ali <farhaan@gro.xyz>
2022-07-09 14:19:19 +0100

    feat: FA - Project setup for GMerkleVestor

---
+++ src/GMerkleVestor.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.10;
+
+import { IERC20 } from 'openzeppelin-contracts/contracts/token/ERC20/IERC20.sol';
+import { SafeERC20 } from 'openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol';
+
+contract GMerkleVestor {}
