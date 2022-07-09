3fb32878a4a6ca268247c33f942ac3eba94aceff
farhaan-ali <farhaan@gro.xyz>
2022-07-09 18:58:28 +0100

    feat: FA - Include example merkle file for testing

--- test/GMerkleVestor.t.sol
+++ test/GMerkleVestor.t.sol
@@ -3,6 +3,23 @@ pragma solidity 0.8.10;
 
 import "forge-std/Test.sol";
 
+// merkle data for testing
+// "root": "0x3d716f83ed930b6b542e68b462bb31ab21c4c52810d9a784e6c92f93bf6d7063"
+//{
+//   "address": "0x022Ce4715b44EF6F0eAd8561B29dA676928D16f3",
+//    "amount": "74221418360000000000",
+//    "proofs": [
+//      "0xf4af01c1cb283bdb99c03e8e21e4d5302449578c64fcedad0265440b2c4f7a30",
+//      "0x2ec282603082d52f0b627ddf7f1171c5b14fcfa10268cfae06e8602ec6e71c47",
+//      "0xf9590d8d50cceec5eb97532e1d6c3b3bee5f8569c5417dd86750d3f44b4187ed",
+//      "0xe4c39decedce6e55b2f9fef6ed6890bba72706f6e4a632e09ee016075f79c907",
+//      "0xf56a479881743dfce47a7d142179ad89df8d29e0404e9dcc36433ad2353ac28f",
+//      "0x26b733621d7e371e99c97e9f5676af8157adcf9da6875924e1bf22691b94792b",
+//      "0x785969f19edd48af29a9ec6f3d7d6b6302d45d50f9df2de7ba0e83d8b5ca1aab",
+//      "0xccc2ae174e184922bfddef5bdfa73dd71f82a6183ebf261fd673b46f0e5f9e75"
+//    ]
+//  }
+
 contract ContractTest is Test {
     function setUp() public {}
 
