e45697ef19ecab03cbeefb78b849a9f436b586d4
farhaan-ali <farhaan@gro.xyz>
2022-07-10 13:51:01 +0100

    test: FA - User can claim full amount if vested period over with
    initial claim.

--- test/unit/GMerkleVestor.t.sol
+++ test/unit/GMerkleVestor.t.sol
@@ -137,7 +137,24 @@ contract GMerkleVestorTest is Test {
     }
 
     function testUserCanClaimFullAmountOnInitialClaim() public {
-
+        uint256 endTimestamp = gmerkle.vestingEndTime();
+        vm.warp(endTimestamp);
+		vm.startPrank(user2);
+		proof = [
+			bytes32(0xf4af01c1cb283bdb99c03e8e21e4d5302449578c64fcedad0265440b2c4f7a30),
+			0x2ec282603082d52f0b627ddf7f1171c5b14fcfa10268cfae06e8602ec6e71c47,
+			0xf9590d8d50cceec5eb97532e1d6c3b3bee5f8569c5417dd86750d3f44b4187ed,
+			0xe4c39decedce6e55b2f9fef6ed6890bba72706f6e4a632e09ee016075f79c907,
+			0xf56a479881743dfce47a7d142179ad89df8d29e0404e9dcc36433ad2353ac28f,
+			0x26b733621d7e371e99c97e9f5676af8157adcf9da6875924e1bf22691b94792b,
+			0x785969f19edd48af29a9ec6f3d7d6b6302d45d50f9df2de7ba0e83d8b5ca1aab,
+			0xccc2ae174e184922bfddef5bdfa73dd71f82a6183ebf261fd673b46f0e5f9e75
+		];
+        uint256 userTotalClaim = 74221418360000000000;
+        gmerkle.initialClaim(proof, userTotalClaim);
+        uint256 userTokenBalance = token.balanceOf(user2);
+        assertEq(userTokenBalance, userTotalClaim);
+        vm.stopPrank();
     }
 
     function testUserCanClaimInitialAmountAndOnGoingClaim() public {
