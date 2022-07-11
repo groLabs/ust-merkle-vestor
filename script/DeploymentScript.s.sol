c582938f1e8f361420e61618fd1b6742a0eea2fd
farhaan-ali <farhaan@gro.xyz>
2022-07-11 19:31:20 +0100

    feat: FA - Update deployment script and add final merkle root

--- script/DeploymentScript.s.sol
+++ script/DeploymentScript.s.sol
@@ -7,14 +7,15 @@ import { GMerkleVestor } from '../src/GMerkleVestor.sol';
 contract DeploymentScript is Script {
 	function run() external {
 		vm.startBroadcast();
-		// TODO update constructor values when deploying to mainnet
-		address token = address(0);
-		uint256 vestingStartTime = 0;
-		bytes32 merkleRoot = bytes32(0);
-
+		// constructor values for gMerkleVestor
+		address token = address(0xF0a93d4994B3d98Fb5e3A2F90dBc2d69073Cb86b);
+		uint256 vestingStartTime = 1656654244;
+		bytes32 merkleRoot = bytes32(0x391ebf4ec9c9c71a62b053ebe97de660302989c7b7e0921d4f548c8b6f425a7c);
+        // deploy contract
 		GMerkleVestor gMerkleVestor = new GMerkleVestor(token, vestingStartTime, merkleRoot);
-
-        // TODO remember to transfer ownership to DAO multisig at the end of script 
+        // transfer ownership to the DAO Multisig
+        address daoMultiSig = address(0x359F4fe841f246a095a82cb26F5819E10a91fe0d);
+        gMerkleVestor.transferOwnership(daoMultiSig);
 
 		vm.stopBroadcast();
 	}
